03d382963f8d3df460665c37a005415b53e90f8b
Philip Nye <philipn@engarts.com>
2013-07-19 09:53:18 +0100

    Add host_parser section.

--- demo/acncfg_local.h
+++ demo/acncfg_local.h
@@ -35,6 +35,10 @@ These configs depend on which demo we are building
 #define ACNCFG_DMP_DEVICE 0
 #define ACNCFG_DMP_CONTROLLER 1
 
+#elif defined(host_parser)
+
+#define ACNCFG_ACNLOG ACNLOG_STDERR
+
 #endif
 
 #endif  /* __acncfg_local_h__ */
