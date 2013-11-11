ad3ec4c1a6b2357bb3d341637b04f2dfcabaa20f
Philip Nye <philipn@engarts.com>
2013-11-11 22:11:37 +0000

    Implement set property functionality Fix bugs

--- demo/controller_demo.c
+++ demo/controller_demo.c
@@ -49,6 +49,15 @@ const char uacn_dflt[] = "Unnamed demo controller";
 #define LIFETIME 300
 
 /**********************************************************************/
+struct dmptcxt_s *ctlcxt = NULL;
+
+/**********************************************************************/
+struct Rcomponent_s *remlist[MAX_REMOTES];
+struct member_s *ctlmbrs[MAX_REMOTES] = {NULL,};
+int nremotes = 0;
+struct uuidset_s devtrees;
+
+/**********************************************************************/
 struct Lcomponent_s localComponent = {
 	.fctn = fctn,
 	.uacn = uacn,
@@ -79,19 +88,71 @@ struct Lcomponent_s localComponent = {
 };
 
 /**********************************************************************/
+const char *evnames[] = {
+	[EV_RCONNECT]         = "remote initiated connect",
+	[EV_LCONNECT]         = "local initiated connect",
+	[EV_DISCOVER]         = "adhoc info",
+	[EV_JOINFAIL]         = "join fail",
+	[EV_JOINSUCCESS]      = "join success",
+	[EV_LOCCLOSE]         = "local initiated close",
+	[EV_LOCDISCONNECT]    = "local initiated disconnect",
+	[EV_LOCDISCONNECTING] = "local initiated disconnecting",
+	[EV_LOCLEAVE]         = "local initiated leave",
+	[EV_LOSTSEQ]          = "lost sequence",
+	[EV_MAKTIMEOUT]       = "MAK timeout",
+	[EV_NAKTIMEOUT]       = "NAK timeout",
+	[EV_REMDISCONNECT]    = "remote initiated disconnect",
+	[EV_REMDISCONNECTING] = "remote initiated disconnecting",
+	[EV_REMLEAVE]         = "remote initiated leave",
+	[EV_CONNECTFAIL]      = "connect fail",
+};
+/**********************************************************************/
 void cd_sdtev(int event, void *object, void *info)
 {
+	struct Lchannel_s *Lchan;
+	struct member_s *mbr;
+	char uuidstr[UUID_STR_SIZE];
+	int i;
 
 	LOG_FSTART();
+	acnlogmark(lgDBUG, "SDT event: %s", evnames[event]);
+
 	switch (event) {
 	case EV_RCONNECT:  /* object = Lchan, info = memb */
 	case EV_LCONNECT:  /* object = Lchan, info = memb */
+		Lchan = (struct Lchannel_s *)object;
+		mbr = (struct member_s *)info;
+
+		if (ctlcxt && Lchan == ctlcxt->dest) {
+			for (i = 0; i < nremotes; ++i) {
+				if (remlist[i] == mbr->rem.Rcomp) {
+					ctlmbrs[i] = mbr;
+					fprintf(stdout, "Remote %i [%.4s] connected\n", i + 1,
+						uuid2str(remlist[i]->uuid, uuidstr));
+				}
+			}
+		}
+		break;
 	case EV_REMDISCONNECT:  /* object = Lchan, info = memb */
 	case EV_LOCDISCONNECT:  /* object = Lchan, info = memb */
+		Lchan = (struct Lchannel_s *)object;
+		mbr = (struct member_s *)info;
+
+		if (ctlcxt && Lchan == ctlcxt->dest) {
+			for (i = 0; i < nremotes; ++i) {
+				if (ctlmbrs[i] == mbr) {
+					ctlmbrs[i] = NULL;
+					fprintf(stdout, "Remote %i [%.6s] disconnected\n", i + 1,
+						uuid2str(remlist[i]->uuid, uuidstr));
+				}
+			}
+		}
+		break;
 	case EV_DISCOVER:  /* object = Rcomp, info = discover data in packet */
 	case EV_JOINSUCCESS:  /* object = Lchan, info = memb */
 	case EV_JOINFAIL:  /* object = Lchan, info = memb->rem.Rcomp */
 	case EV_LOCCLOSE:  /* object = , info =  */
+	case EV_LOCDISCONNECTING:  /* object = Lchan, info = memb */
 	case EV_LOCLEAVE:  /* object = Lchan, info = memb */
 	case EV_LOSTSEQ:  /* object = Lchan, info = memb */
 	case EV_MAKTIMEOUT:  /* object = Lchan, info = memb */
@@ -104,12 +165,6 @@ void cd_sdtev(int event, void *object, void *info)
 }
 
 /**********************************************************************/
-struct Rcomponent_s *remlist[MAX_REMOTES];
-struct member_s *ctlmbrs[MAX_REMOTES] = {NULL,};
-int nremotes = 0;
-struct uuidset_s devtrees;
-
-/**********************************************************************/
 static bool
 wordmatch(char **str, const char *word, int minlen)
 {
@@ -118,49 +173,46 @@ wordmatch(char **str, const char *word, int minlen)
 	bp = *str;
 	for (bp = *str; *word && *bp == *word; ++bp, ++word, --minlen);
 
-	if ((*bp == 0 || *bp == ' ') && (*word == 0 || minlen <= 0)) {
+	if ((*bp == 0 || isspace(*bp)) && (*word == 0 || minlen <= 0)) {
 		*str = bp;
 		return true;
 	}
 	return false;
 }
 /**********************************************************************/
-int
-readint(char **bpp, int *ip, int min, int max)
+static int
+readU32(char **bpp, uint32_t *ip)
 {
 	char *bp, *cp;
-	long l;
+	uint32_t l;
 	
 	bp = *bpp;
-	l = strtol(bp, &cp, 0);
+	l = strtoul(bp, &cp, 0);
 	if (cp == bp || (*cp != 0 && !isspace(*cp))) {
 		return -1;
-	}
-	if (l < min || l > max) {
-		return -2;
 	}
 	*ip = l;
 	*bpp = cp;
 	return 0;
 }
+
+#define readS32(bpp, lp) readU32(bpp, (unsigned long *)(lp))
 /**********************************************************************/
 int
 readremote(char **bpp)
 {
-	int rem;
+	uint32_t rem;
 
-	switch (readint(bpp, &rem, 1, nremotes)) {
-	case -1:
+	if (readU32(bpp, &rem) < 0) {
 		fprintf(stdout, "Please specify a remote device by number\n");
 		return -1;
-	case -2:
-		fprintf(stdout, "Specified remote number is out of range\n");
-		/* fall through */
-	default:
-		return -1;
-	case 0:
-		return rem - 1;
 	}
+	--rem;  /* change to zero-based */
+	if (rem >= nremotes) {
+		fprintf(stdout, "Specified remote number is out of range\n");
+		return -1;
+	}
+	return (int)rem;
 }
 /**********************************************************************/
 void
@@ -170,13 +222,14 @@ ddltree(char **bpp)
 	const struct rootdev_s *root;
 	const uint8_t *rootdcid;
 	int rem;
+	int i;
 
 	if ((rem = readremote(bpp)) < 0) return;
 	Rcomp = remlist[rem];
 	
-	rootdcid = finduuid(&devtrees, Rcomp->slp.dcid);
-	root = container_of(rootdcid, struct rootdev_s, dcid[0]);
-	if (root == NULL) {
+	if ((rootdcid = finduuid(&devtrees, Rcomp->slp.dcid)) != NULL) {
+		root = container_of(rootdcid, struct rootdev_s, dcid[0]);
+	} else {
 		char dcidstr[UUID_STR_SIZE];
 		union addrmap_u *amap;
 		uint32_t adrange;
@@ -189,19 +242,38 @@ ddltree(char **bpp)
 			acnlog(lgERR, "Can't generate device %.8s...", dcidstr);
 			return;
 		}
+		acnlog(lgDBUG, "Add new DCID %.8s...", dcidstr);
+		rootdcid = root->dcid;
 		adduuid(&devtrees, root->dcid);
 		amap = root->amap;
+		assert(amap != NULL);
 		adrange = root->maxaddr - root->minaddr;
 		ixratio = (adrange * sizeof(void *))
 				/ (amap->srch.count * sizeof(struct addrfind_s));
 
+		acnlog(lgDBUG, "Address range %u, index:search map size ratio %u",
+				adrange, ixratio);
 		if (adrange <= 64 || ixratio < 3) {
 			/* other criteria for conversion could be used */
-			acnlogmark(lgDBUG, "Transforming map\n");
+			acnlogmark(lgDBUG, "Transforming map");
 			xformtoindx(amap);
+		} else {
+			acnlogmark(lgDBUG, "Using search map");
 		}
-		Rcomp->dmp.amap = amap;
 	}
+	acnlog(lgDBUG, "Assign new map to all devices of this DCID");
+	for (i = 0; i < nremotes; ++i) {
+		Rcomp = remlist[i];
+		assert(Rcomp && Rcomp->slp.dcid);
+		acnlogmark(lgDBUG, "try %d \"%s\"...", i, Rcomp->slp.uacn);
+		if (Rcomp->dmp.amap == NULL
+			&& uuidsEq(rootdcid, Rcomp->slp.dcid))
+		{
+			acnlogmark(lgDBUG, "...assigning");
+			Rcomp->dmp.amap = root->amap;
+		}
+	}
+	acnlog(lgDBUG, "all assigned");
 	fprintf(stdout,
 			"DDL tree for device%3u\n"
 			"----------------------\n"
@@ -216,6 +288,7 @@ dodiscover()
 	struct Rcomponent_s *Rcomp;
 	int i;
 	char uuidstr[UUID_STR_SIZE];
+	const uint8_t *rootdcid;
 
 	nremotes = 0;
 
@@ -241,7 +314,7 @@ dodiscover()
 				ctyp = "";
 				break;
 			}
-			fprintf(stdout, "%3i:  %s %s \"%s\" at %s:%-5d  [%.8s]\n",
+			fprintf(stdout, "%3i:  %s %s \"%s\" at %s:%-5d  [%.4s]\n",
 				i + 1,
 				Rcomp->slp.fctn,
 				ctyp,
@@ -250,6 +323,13 @@ dodiscover()
 				ntohs(netx_PORT(&Rcomp->sdt.adhocAddr)),
 				uuid2str(Rcomp->uuid, uuidstr)
 			);
+			rootdcid = finduuid(&devtrees, Rcomp->slp.dcid);
+			if (rootdcid != NULL) {
+				const struct rootdev_s *root;
+
+				root = container_of(rootdcid, struct rootdev_s, dcid[0]);
+				Rcomp->dmp.amap = root->amap;
+			}
 			if (i < MAX_REMOTES) {
 				remlist[i] = Rcomp;
 			}
@@ -260,9 +340,6 @@ dodiscover()
 	}
 }
 /**********************************************************************/
-struct dmptcxt_s *ctlcxt = NULL;
-
-/**********************************************************************/
 void
 dmpconnect(char **bpp)
 {
@@ -270,6 +347,7 @@ dmpconnect(char **bpp)
 	struct member_s *mbr;
 	int i;
 
+	LOG_FSTART();
 	if ((rem = readremote(bpp)) < 0) return;
 	if ((mbr = ctlmbrs[rem]) == NULL) {
 		struct Rcomponent_s *Rcomp;
@@ -285,12 +363,14 @@ dmpconnect(char **bpp)
 				acnlogmark(lgNTCE, "Unable to open control channel");
 				return;
 			}
-			ctlcxt->wflags = 0;
+			ctlcxt->wflags = WRAP_ALL_MEMBERS;
 			ctlcxt->dest = ctlchan;
+		} else if (ctlcxt->wflags & WRAP_ALL_MEMBERS) {
+			ctlchan = (struct Lchannel_s *)ctlcxt->dest;
 		} else {
-			ctlchan = ctlcxt->dest;
+			ctlchan = ((struct member_s *)ctlcxt->dest)->rem.Lchan;
 		}
-		
+
 		Rcomp = remlist[rem];
 		i = addMember(ctlchan, Rcomp);
 		if (i < 0) return;
@@ -299,7 +379,159 @@ dmpconnect(char **bpp)
 	} else {
 		fprintf(stdout, "Remote %d is already connected\n", rem);
 	}
+	LOG_FEND();
 }
+/**********************************************************************/
+void
+setintprop(
+	struct member_s *mbr,
+	const struct dmpprop_s *dprop, 
+	uint32_t addr,
+	uint32_t pval
+)
+{
+	struct adspec_s ads;
+	uint8_t *txp;
+
+	LOG_FSTART();
+	ads.inc = ads.count = 1;
+	ads.addr = addr;
+
+	assert(ctlcxt->pdup == NULL);
+	ctlcxt->dest = mbr;
+	ctlcxt->wflags = WRAP_REL_ON;
+	txp = dmp_openpdu(ctlcxt, DMP_SET_PROPERTY << 8 | DMPAD_SINGLE,
+							&ads, dprop->size);
+	switch (dprop->size) {
+	case 1:
+		txp = marshalU8(txp, (uint8_t)pval);
+		break;
+	case 2:
+		txp = marshalU16(txp, (uint16_t)pval);
+		break;
+	case 4:
+		txp = marshalU32(txp, pval);
+		break;
+	default:
+		acnlogmark(lgERR, "Unsupported integer property size %u", dprop->size);
+		dmp_abortblock(ctlcxt);
+		return;
+	}
+	dmp_closeflush(ctlcxt, txp);
+	LOG_FEND();
+}
+/**********************************************************************/
+void
+setstringprop(
+	struct member_s *mbr,
+	const struct dmpprop_s *dprop,
+	uint32_t addr,
+	char *str,
+	int len
+)
+{
+	struct adspec_s ads;
+	uint8_t *txp;
+
+	LOG_FSTART();
+	ads.inc = ads.count = 1;
+	ads.addr = addr;
+	ctlcxt->wflags = WRAP_REL_ON;
+	acnlogmark(lgERR, "Set string prop %u to \"%.*s\"", addr, len, str);
+
+	assert(ctlcxt->pdup == NULL);
+	ctlcxt->dest = mbr;
+	txp = dmp_openpdu(ctlcxt, DMP_SET_PROPERTY << 8 | DMPAD_SINGLE,
+							&ads, len + 2);
+	txp = marshalVar(txp, (uint8_t *)str, len);
+	dmp_closeflush(ctlcxt, txp);
+	LOG_FEND();
+}
+/**********************************************************************/
+void
+setprop(char **bpp)
+{
+	int rem;
+	struct member_s *mbr;
+	uint32_t addr;
+	union addrmap_u *amap;
+	const struct dmpprop_s *dprop;
+
+	if ((rem = readremote(bpp)) < 0) return;
+	if ((mbr = ctlmbrs[rem]) == NULL) {
+		fprintf(stdout, "Remote %i \"%s\" is not connected\n", rem,
+				remlist[rem]->slp.uacn);
+		return;
+	}
+	if ((amap = remlist[rem]->dmp.amap) == NULL) {
+		fprintf(stdout, "Remote %i \"%s\": check DDL\n", rem, 
+					remlist[rem]->slp.uacn);
+		return;
+	}
+	if (readU32(bpp, &addr) < 0) {
+		fprintf(stdout, "Must specify a property number\n");
+		return;
+	}
+	if ((dprop = addr_to_prop(amap, addr)) == NULL
+		|| !(dprop->flags & pflg(write))) {
+		fprintf(stdout, "No such property or not writable\n");
+		return;
+	}
+	switch (dprop->etype) {
+	case etype_enum:
+	case etype_uint:
+	case etype_sint: {
+		uint32_t pval;
+
+		if (readU32(bpp, &pval) < 0) {
+			fprintf(stdout, "Integer property needed\n");
+			return;
+		}
+		setintprop(mbr, dprop, addr, pval);
+		break;}
+	case etype_string: {
+		char *cp, *ep;
+
+		cp = *bpp;
+		while (isspace(*cp)) ++cp;
+		if (*cp == '"') {
+			for (ep = ++cp; *ep && *ep != '"'; ++ep);
+		} else {
+			for (ep = cp; *ep && !isspace(*ep); ++ep);
+		}
+		*bpp = ep;
+
+		setstringprop(mbr, dprop, addr, cp, (int)(ep - cp));
+		break;}
+	case etype_unknown:
+	case etype_boolean:
+	case etype_float:
+	case etype_UTF8:
+	case etype_UTF16:
+	case etype_UTF32:
+	case etype_opaque:
+	case etype_uuid:
+	case etype_bitmap:
+		fprintf(stdout, "%s property unsupported\n", etypes[dprop->etype]);
+		break;
+	}
+}
+/*
+void
+dmpdisconnect(char **bpp)
+{
+	int rem;
+	struct member_s *mbr;
+	int i;
+
+	if ((rem = readremote(bpp)) < 0) return;
+	if ((mbr = ctlmbrs[rem]) == NULL) {
+		fprintf(stdout, "Not connected\n");
+		return;
+	}
+}
+*/
+
 /**********************************************************************/
 void
 term_event(uint32_t evf, void *evptr)
@@ -324,6 +556,8 @@ term_event(uint32_t evf, void *evptr)
 		ddltree(&bp);
 	else if (wordmatch(&bp, "connect", 2))
 		dmpconnect(&bp);
+	else if (wordmatch(&bp, "setproperty", 2) || wordmatch(&bp, "sp", 2))
+		setprop(&bp);
 	else if (wordmatch(&bp, "quit", 1)) runstate = rs_quit;
 	else {
 		fprintf(stdout, "Bad command \"%s\"\n", bp);
@@ -357,28 +591,23 @@ run_controller(const char *uuidstr, uint16_t port, const char **interfaces)
 	/* prepare DMP */
 	if (dmp_register() < 0) {
 		acnlogerror(lgERR);
-	} else if (sdt_register(&cd_sdtev) < 0) {
+	} else if (sdt_register(&cd_sdtev, &listenaddr, ADHOCJOIN_NONE) < 0) {
 		acnlogerror(lgERR);
 	} else {
-		if (sdt_setListener(NULL, &listenaddr) < 0) {
+		if (sdt_addClient(&dmp_sdtRx, NULL) < 0) {
 			acnlogerror(lgERR);
 		} else {
-			if (sdt_addClient(&dmp_sdtRx, NULL) < 0) {
-				acnlogerror(lgERR);
-			} else {
-				/* now we can advertise ourselves */
-				acnlogmark(lgDBUG, "starting SLP");
-				slp_register();
-			
-				evl_register(STDIN_FILENO, &term_event_ref, EPOLLIN);
+			/* now we can advertise ourselves */
+			acnlogmark(lgDBUG, "starting SLP");
+			slp_register();
+		
+			evl_register(STDIN_FILENO, &term_event_ref, EPOLLIN);
 
-				evl_wait();
-			
-				evl_register(STDIN_FILENO, NULL, 0);
+			evl_wait();
+		
+			evl_register(STDIN_FILENO, NULL, 0);
 
-				slp_deregister();
-			}
-			sdt_clrListener();
+			slp_deregister();
 		}
 		sdt_deregister();
 	}
