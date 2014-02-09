e71eeb473eeb7da63174b37a4cbd3344de529cb3
Philip Nye <philipn@engarts.com>
2014-02-09 10:58:15 +0000

    Move mkcfg.h from main source into demo and adjust Makefile.

--- demo/mkcfg.h
+++ demo/mkcfg.h
@@ -1,3 +1,30 @@
+/**********************************************************************/
+/*
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+Copyright (c) 2013, Acuity Brands, Inc.
+
+Author: Philip Nye <philip.nye@engarts.com>
+
+This file forms part of Acacian a full featured implementation of 
+ANSI E1.17 Architecture for Control Networks (ACN)
+
+#tabs=3
+*/
+/**********************************************************************/
+/*
+header: mkcfg.h
+
+Header used co generate <prog>-cfg.mak summary of configuration
+in Makefile format.
+
+note:
+This header is processed by the C preprocessor but is not compiled and
+is not valic C code.
+*/
+
 #include "acn.h"
 
 @_CF_VERSION CF_VERSION
