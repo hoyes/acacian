4e07ccd7d5f27873304cb1976d58e2021e145c7c
Philip Nye <philipn@engarts.com>
2014-02-09 10:57:42 +0000

    Add explanatory header and copyright notice.

--- include/expat_config.h
+++ include/expat_config.h
@@ -1,4 +1,30 @@
+/**********************************************************************/
+/*
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+Copyright (c) 2013, Acuity Brands, Inc.
+
+Author: Philip Nye <philip.nye@engarts.com>
+
+This file forms part of Acacian a full featured implementation of 
+ANSI E1.17 Architecture for Control Networks (ACN)
+
+#tabs=3
+*/
+/**********************************************************************/
+/*
+header: expat_config.h
+
+Acacian configuration for expat when it is built-in.
+*/
+#ifndef __expat_config_h__
+#define __expat_config_h__ 1
+
 #define HAVE_MEMMOVE 1
 
 #define XML_NS 1
 #undef XML_DTD
+
+#endif  /* __expat_config_h__ */
